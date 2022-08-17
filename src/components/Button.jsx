e33971d014d702b243e30f160718191c014e5b10
HiepFK <hiepnh.fk@gmail.com>
2022-08-18 00:44:52 +0700

    update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,13 +4,13 @@ function Button({ nextUrl, preUrl, setLink }) {
   return (
     <div className="flex items-center justify-center pb-6">
       <div
-        className="bg-amber-500 flex items-center justify-center w-20 h-8	rounded ml-1 mr-1 cursor-pointer"
+        className="bg-amber-500 flex items-center justify-center w-20 h-8 rounded ml-1 mr-1 cursor-pointer"
         onClick={() => setLink(preUrl)}
       >
         Previous
       </div>
       <div
-        className="bg-amber-500 flex items-center justify-center w-20 h-8	rounded ml-1 mr-1 cursor-pointer"
+        className="bg-amber-500 flex items-center justify-center w-20 h-8 rounded ml-1 mr-1 cursor-pointer"
         onClick={() => setLink(nextUrl)}
       >
         Next
