3a09f9cf8327a81ec284397d7544c06417b400e1
HiepFK <hiepnh.fk@gmail.com>
2022-08-21 00:27:46 +0700

    update id

--- src/apis/evolution.js
+++ src/apis/evolution.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+const deleteUrl = "https://pokeapi.co/api/v2/pokemon-species/";
 export const getEvolution = async (url) => {
   let pokemon1 = { name: "", url: "" };
   let pokemon2 = { name: "", url: "" };
@@ -9,23 +9,23 @@ export const getEvolution = async (url) => {
   dataEvolution = res.data;
   if (dataEvolution?.chain?.species) {
     pokemon1.name = dataEvolution?.chain?.species?.name;
-    pokemon1.url = dataEvolution?.chain?.species?.url?.slice(42, 43);
+    pokemon1.url = dataEvolution?.chain?.species?.url
+      ?.replace(deleteUrl, "")
+      .replace("/", "");
   }
   if (dataEvolution?.chain?.evolves_to?.[0]?.species) {
     pokemon2.name = dataEvolution?.chain?.evolves_to?.[0]?.species?.name;
-    pokemon2.url = dataEvolution?.chain?.evolves_to?.[0]?.species?.url?.slice(
-      42,
-      43
-    );
+    pokemon2.url = dataEvolution?.chain?.evolves_to?.[0]?.species?.url
+      ?.replace(deleteUrl, "")
+      .replace("/", "");
 
     if (dataEvolution?.chain?.evolves_to?.[0]?.evolves_to?.[0]?.species?.url) {
       pokemon3.name =
         dataEvolution?.chain?.evolves_to?.[0]?.evolves_to?.[0]?.species?.name;
       pokemon3.url =
-        dataEvolution?.chain?.evolves_to?.[0]?.evolves_to?.[0]?.species?.url?.slice(
-          42,
-          43
-        );
+        dataEvolution?.chain?.evolves_to?.[0]?.evolves_to?.[0]?.species?.url
+          ?.replace(deleteUrl, "")
+          .replace("/", "");
     }
   }
   return { pokemon1, pokemon2, pokemon3 };
--- src/components/Evolution.jsx
+++ src/components/Evolution.jsx
@@ -5,42 +5,57 @@ function Evolution({ evolution }) {
   console.log(evolution);
   return (
     <div className="flex flex-col justify-around items-center md:flex-row">
-      <div className="flex flex-col justify-center items-center p-4">
-        <img
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon1?.url}.png`}
-          alt=""
-          className="w-60 rounded-full shadow-xl cursor-pointer bg-gradient-to-r from-amber-500 to-yellow-400 hover:from-green-500 hover:to-yellow-400 p-5"
-        />
+      <div className="flex flex-col justify-center items-center p-4 ">
+        <div className="rounded-full w-60  shadow-xl cursor-pointer bg-gradient-to-r from-amber-500 to-yellow-400 hover:from-green-500 hover:to-yellow-400">
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon1?.url}.png`}
+            alt=""
+            className="p-6"
+          />
+        </div>
         <div className="text-base	font-medium capitalize mt-2">
           {pokemon1?.name}
         </div>
       </div>
-      <div>
-        <img src={img} alt="" className="rotate-90 w-24 md:rotate-0" />
-      </div>
-      <div className="flex flex-col justify-center items-center  p-4">
-        <img
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon2?.url}.png`}
-          alt=""
-          className="w-60 rounded-full shadow-xl cursor-pointer bg-gradient-to-r from-amber-500 to-yellow-400 hover:from-green-500 hover:to-yellow-400 p-5"
-        />
-        <div className="text-base	font-medium capitalize mt-2">
-          {pokemon2?.name}
-        </div>
-      </div>
-      <div>
-        <img src={img} alt="" className="rotate-90 w-24 md:rotate-0" />
-      </div>
-      <div className="flex flex-col justify-center items-center p-4">
-        <img
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon3?.url}.png`}
-          alt=""
-          className="w-60 rounded-full shadow-xl cursor-pointer bg-gradient-to-r from-amber-500 to-yellow-400 hover:from-green-500 hover:to-yellow-400 p-5"
-        />
-        <div className="text-base	font-medium capitalize mt-2">
-          {pokemon3?.name}
-        </div>
-      </div>
+      {pokemon2?.url && (
+        <>
+          <div>
+            <img src={img} alt="" className="rotate-90 w-24 md:rotate-0" />
+          </div>
+          <div className="flex flex-col justify-center items-center  p-4">
+            <div className="w-60 rounded-full shadow-xl cursor-pointer bg-gradient-to-r from-amber-500 to-yellow-400 hover:from-green-500 hover:to-yellow-400">
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon2?.url}.png`}
+                alt=""
+                className=" p-6"
+              />
+            </div>
+            <div className="text-base	font-medium capitalize mt-2">
+              {pokemon2?.name}
+            </div>
+          </div>
+        </>
+      )}
+
+      {pokemon3?.url && (
+        <>
+          <div>
+            <img src={img} alt="" className="rotate-90 w-24 md:rotate-0" />
+          </div>
+          <div className="flex flex-col justify-center items-center p-4">
+            <div className="w-60 rounded-full shadow-xl cursor-pointer bg-gradient-to-r from-amber-500 to-yellow-400 hover:from-green-500 hover:to-yellow-400">
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon3?.url}.png`}
+                alt=""
+                className=" p-6"
+              />
+            </div>
+            <div className="text-base	font-medium capitalize mt-2">
+              {pokemon3?.name}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
